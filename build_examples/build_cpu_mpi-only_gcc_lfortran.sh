b349cd53b104916fda9fa0620c3576e720f587e7
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-02-05 13:18:14 +0530

    build: add LFortran build script

---
+++ build_examples/build_cpu_mpi-only_gcc_lfortran.sh
@@ -0,0 +1,19 @@
+# build script which uses LFortran as it's compiler
+# along with using our own MPI wrappers and call C MPI instead
+# and doesn't use any HDF5
+POT3D_HOME=$PWD
+MPICC=${CONDA_PREFIX}/bin/mpicc
+FC=${FC:-"../src/bin/lfortran"}
+FFLAGS="-lmpi"
+
+echo "==> Using Fortran compiler: " ${FC}
+echo "=== Starting build process ==="
+
+echo "Entering src directory"
+cd src
+${MPICC} -c mpi_wrapper.c
+${FC} -c ${FFLAGS} mpi_c_bindings.f90
+${FC} -c ${FFLAGS} mpi.f90
+${FC} -c ${FFLAGS} psi_io.f90
+${FC} -E ${FFLAGS} --cpp pot3d.F90 > pot3d_cpp.f90
+${FC} -c ${FFLAGS} --implicit-interface pot3d_cpp.f90 --show-asr
