9f8cf8d70a756cb9bb8550a622c5c5e19d2903e9
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-02-05 17:50:10 +0530

    build: improve LFortran shell script

--- build_examples/build_cpu_mpi-only_gcc_lfortran.sh
+++ build_examples/build_cpu_mpi-only_gcc_lfortran.sh
@@ -12,8 +12,34 @@ echo "=== Starting build process ==="
 echo "Entering src directory"
 cd src
 ${MPICC} -c mpi_wrapper.c
+# even if the commands fail, they don't necessarily exit with non-zero status
+if [ ! -f mpi_wrapper.o ]; then
+    echo "Error: mpi_wrapper.o not created!" >&2
+    exit 1
+fi
+
 ${FC} -c ${FFLAGS} mpi_c_bindings.f90
+if [ ! -f mpi_c_bindings.o ]; then
+    echo "Error: mpi_c_bindings.o not created!" >&2
+    exit 1
+fi
+
 ${FC} -c ${FFLAGS} mpi.f90
+if [ ! -f mpi.o ]; then
+    echo "Error: mpi.o not created!" >&2
+    exit 1
+fi
+
 ${FC} -c ${FFLAGS} psi_io.f90
+if [ ! -f psi_io.o ]; then
+    echo "Error: psi_io.o not created!" >&2
+    exit 1
+fi
+
 ${FC} -E ${FFLAGS} --cpp pot3d.F90 > pot3d_cpp.f90
+if [ ! -f pot3d_cpp.f90 ]; then
+    echo "Error: pot3d_cpp.f90 not created!" >&2
+    exit 1
+fi
+
 ${FC} -c ${FFLAGS} --implicit-interface pot3d_cpp.f90 --show-asr
