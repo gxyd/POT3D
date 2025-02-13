24be0a5a34a8584d5aa9b985db08d6db7990ba57
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-02-13 14:58:19 +0530

    build: do comparison in the validation script

--- validate_lf.sh
+++ validate_lf.sh
@@ -1,4 +1,4 @@
-set -ex
+#!/bin/bash
 
 POT3D_HOME=$PWD
 TEST="validation"
@@ -14,3 +14,29 @@ echo "Done!"
 runtime=($(tail -n 5 timing.out | head -n 1))
 echo "Wall clock time:                ${runtime[6]} seconds"
 echo " "
+
+#Validate run:
+${POT3D_HOME}/scripts/pot3d_validation.sh pot3d.out ${POT3D_HOME}/testsuite/${TEST}/validation/pot3d.out
+if [ $? -ne 0 ]; then
+  echo "Validation failed for 1 MPI rank. Exiting..."
+  exit 1
+fi
+
+# rm pot3d.log pot3d.out timing.out
+# echo " "
+# echo "Running POT3D with 2 MPI ranks..."
+# ${MPIEXEC} -np 2 ${POT3D_HOME}/bin/pot3d 1> pot3d.log 2>pot3d.err
+# echo "Done!"
+
+# runtime=($(tail -n 5 timing.out | head -n 1))
+# echo "Wall clock time:                ${runtime[6]} seconds"
+# echo " "
+
+# #Validate run:
+# ${POT3D_HOME}/scripts/pot3d_validation.sh pot3d.out ${POT3D_HOME}/testsuite/${TEST}/validation/pot3d.out
+# if [ $? -ne 0 ]; then
+#   echo "Validation failed for 1 MPI rank. Exiting..."
+#   exit 1
+# fi
+# echo " "
+
