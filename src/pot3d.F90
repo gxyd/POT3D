02d74dcf8384f9c44b6dbcc809bd62800b87b75e
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-02-27 08:58:47 +0530

    UNDO XX Revert "XX: workaround to use another array instead of passing an array section (#54)" (#66)
    This reverts commit fbb69c23244911f077489f6a2ef2cd9214138a14.

--- src/pot3d.F90
+++ src/pot3d.F90
@@ -6720,7 +6720,6 @@ subroutine readbr (fname,br0_g,ierr)
 !
       character(*) :: fname
       real(r_typ), dimension(nt_g,np_g) :: br0_g
-      real(r_typ), dimension(:, :), allocatable :: f
       integer :: ierr
 !
 !-----------------------------------------------------------------------
@@ -6775,14 +6774,10 @@ subroutine readbr (fname,br0_g,ierr)
 !
 ! ****** Interpolate the field to the code mesh (into array BR0_G).
 !
-      ! TODO: XX: remove the workaround
-      allocate(f(ntm1_g - 1, npm1_g-1))
-      f = br0_g(2:ntm1_g, 2:npm1_g)
       call intrp2d (ntn,npn,tn,pn,bn, &
                     nt_g-2,np_g-2,th_g(2:ntm1_g),ph_g(2:npm1_g), &
-                    f,ierr)
+                    br0_g(2:ntm1_g,2:npm1_g),ierr)
 !
-      br0_g(2:ntm1_g, 2:npm1_g) = f
       if (ierr.ne.0) then
         write (*,*)
         write (*,*) '### ERROR in READBR:'
