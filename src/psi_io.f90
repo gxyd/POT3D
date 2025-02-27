e3e91048e4e15078520966f079ef02ba5d4075d1
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-02-27 09:05:16 +0530

    UNDO XX Revert "XX: workaround, no need to use 'contiguous' attribute"

--- src/psi_io.f90
+++ src/psi_io.f90
@@ -33,7 +33,7 @@ module rp1d_def
       implicit none
 !
       type :: rp1d
-        real(REAL64), dimension(:), pointer :: f
+        real(REAL64), dimension(:), pointer, contiguous :: f
       end type
 !
 end module
@@ -58,7 +58,7 @@ module sds_def
         logical :: hdf32
       !   type(rp1d), dimension(mxdim) :: scales
         real(REAL64), dimension(:,:), pointer :: scales
-        real(REAL64), dimension(:,:,:), pointer :: f
+        real(REAL64), dimension(:,:,:), pointer, contiguous :: f
       end type
 !
 end module
